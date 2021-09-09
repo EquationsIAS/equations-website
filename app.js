0e8223974321e8ae95602d48019fb54b9fcf6d63
DevDeepak-Arya <aryadeepak087.da@gmail.com>
2021-09-09 22:10:51 +0530

    added new file

---
+++ app.js
@@ -0,0 +1,11 @@
+const navSlide = () =>{
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links'); 
+    const navLinks = document.querySelectorAll('.nav-links li')
+    //Toggle nav
+    burger.addEventListener('click',() => {
+        nav.classList.toggle('nav-active');
+    });  
+ }
+ 
+ navSlide();

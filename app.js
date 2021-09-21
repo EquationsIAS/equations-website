e33c6e5ef8821d4345f66e8946cd26c2028886f9
DevDeepak-Arya <aryadeepak087.da@gmail.com>
2021-09-21 12:31:29 +0530

    course section and more

--- app.js
+++ app.js
@@ -1,11 +1,148 @@
 const navSlide = () =>{
     const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links'); 
+    const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li')
     //Toggle nav
     burger.addEventListener('click',() => {
         nav.classList.toggle('nav-active');
-    });  
+    });
  }
- 
+
  navSlide();
+ $('.slider').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1299,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 954,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint:840,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+
+  $('.course-slider').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    nextArrow:$('.next'),prevArrow:$('.prev'),
+    responsive: [
+      {
+        breakpoint: 1299,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: false,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 954,
+        settings: {
+          slidesToShow:2,
+          slidesToScroll: 1,
+          infinite: false,
+          dots: true
+        }
+      },
+
+      {
+        breakpoint:840,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: false,
+          dots: true
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+$('.post-wrapper').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  nextArrow:$('.next-perk'),prevArrow:$('.prev-perk'),
+    responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll:1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+          infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,  infinite: true,
+        dots: true
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+
+
+});
+
+  // Product Filter
+    $(document).ready(function(){
+      $('.course-link').click(function(){
+        const value=$(this).attr('data-filter');
+        if(value == 'all'){
+          $('.course-card-1').show('1000');
+        }
+        else{
+          $('.course-card-1').not('.'+value).hide('1000');
+          $('.course-card-1').filter('.'+value).show('1000');
+        }
+      })
+      //add active classes on selected items
+      $('.course-link').click(function(){
+        $(this).addClass('active').siblings().removeClass('active');
+      })
+    })
